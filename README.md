460b7ade620a97183d8397646aecb6049ddab61b
Louis Maddox <lmmx@users.noreply.github.com>
2021-12-26 08:36:53 +0000

    Locally reproducible demo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pdf2uplambda
 
 An AWS Lambda function for pdf2up (2021)
+
+Reproducible on replit but requires poppler (replit doesn't seem to support)
--- lambda_function.py
+++ lambda_function.py
@@ -1,38 +1,52 @@
 from __future__ import annotations
+
 import argparse
 import json
-import pdf2up
+from pathlib import Path
 from urllib.parse import urlparse
 
+import boto3
+import pdf2up
+import httpx
 
-def prepare_arxiv_url(arx_id: str) -> str:
+
+def id_from_arx_url(url: str) -> str:
+    parsed = urlparse(url)
+    subpath = Path(parsed.path)
+    pdf_suff = subpath.suffix == "pdf"
+    arx_id = subpath.stem if pdf_suff else subpath.name
+    return arx_id
+
+
+def arx_url_from_id(arx_id: str) -> str:
     """
     Prepare a URL at the proper export subdomain of arxiv.org so that PDF requests don't return a 403 error when pulled by a non-browser viewer.
     """
     return f"https://export.arxiv.org/{arx_id}.pdf"
 
-def id_from_arx_url(url: str) -> str:
-    parsed = urlparse(url)
-    subpath = Path(parsed.path)
-    pdf_suff = subpath.suffix == 'pdf'
-    arx_id = subpath.stem if pdf_suff else subpath.name
-    return arx_id
 
 def lambda_handler(event, context=None):
     print(type(event))
     print(event)
-    if "url" not in event:
+    if not (url := event.get("url")):
         raise ValueError("Expected a URL")
     parsed = urlparse(url)
-    exp_loc = 'export.arxiv.org'
+    exp_loc = "export.arxiv.org"
     loc_check = parsed.netloc != exp_loc
     pdf_suff = parsed.path.endswith(".pdf")
     if not (loc_check and pdf_suff):
-        if parsed.netloc != 'arxiv.org':
+        if parsed.netloc != "arxiv.org":
             raise ValueError("Not an arXiv URL")
         arx_id = id_from_arx_url(url)
         url = arx_url_from_id(arx_id)
     req = httpx.get(url)
+    req.raise_for_status()
+    out_path = Path() / f"{arx_id}.pdf"
+    out_path.write_bytes(req.content)
+    s3 = boto3.resource("s3")
+    key=str(out_path.resolve())
+    s3.upload_file(bucket="bucket-name", key=key)
+
 
 
 if __name__ == "__main__":
